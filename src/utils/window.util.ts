0e22ef4dfe4239ee750968520551db16adb3c227
kadev2176 <kevinchristopher2176@gmail.com>
2023-04-26 13:27:59 +0800

    MOD: generate tweet modal ui

--- src/utils/window.util.ts
+++ src/utils/window.util.ts
@@ -5,7 +5,8 @@ export const parseUrlParams = () => {
   try {
     const url = new URL(window.location.href);
     return {
-      ...qs.parse(url.search.substring(1))
+      ...qs.parse(url.search.substring(1)),
+      ...qs.parse(url.hash.substring(1))
     }
   } catch (e) {
     console.log('Parse url params error', e);
